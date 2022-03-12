a09ceb6caa410eac42f6520dc094810ff78feb55
Geize Kele <ge.kele@hotmail.com>
2022-03-12 12:03:30 -0300

    desafio 1

---
+++ 1. Solve Me First/ConsoleApp3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Solution
+{
+    static int solveMeFirst(int a, int b)
+    {
+        // Dica: Digite return a+b abaixo  
+        return a + b;
+    }
+
+    static void Main(String[] args)
+    {
+        Console.Write("Digite o primeiro valor: "); //Linha descartada no desafio
+        int valor1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Digite o segundo valor: "); //Linha descartada no desafio
+        int valor2 = Convert.ToInt32(Console.ReadLine());
+
+        int soma = solveMeFirst(valor1, valor2);
+        Console.WriteLine("A soma é " + soma);
+    }
+}
---
+++ 1. Solve Me First/readme-desafio1.txt
@@ -0,0 +1,4 @@
+Primeiro desafio HackerRank - C#
+Dificuldade: Fácil
+
+.Complete a função solveMeFirst para calcular a soma de dois inteiros. 
